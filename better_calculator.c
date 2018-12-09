70f9dbaf020980f053354b0277a5d88fcdb16e9b
Ky Nguyen <kynguyen@kynguyen-macbookpro.roam.corp.google.com>
2018-12-09 01:58:09 -0800

    adds better calculator

---
+++ better_calculator.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    double num1;
+    double num2;
+    char operator;
+
+    printf("Enter a number: ");
+    scanf("%lf", &num1);
+    printf("Enter an operator: ");
+    scanf(" %c", &operator);
+    printf("Enter another number: ");
+    scanf("%lf", &num2);
+
+    
+    if (operator == '+') {
+        printf("%f", num1 + num2);
+    } 
+    else if (operator == '-')   
+    {
+        printf("%f", num1 - num2);
+    } 
+    else if (operator == '/')   
+    {
+        printf("%f", num1 / num2);
+    } 
+    else if (operator == '*')
+    {
+        printf("%f", num1 * num2);
+    } 
+    else
+    {
+        printf("Invalid Operator");
+    }
+
+    return 0;
+}
